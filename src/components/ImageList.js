16053fd78d3f6b763b43c3a34db68662f9f3da8e
Abhijeet <dev1.parsa@gmail.com>
2020-06-21 15:01:05 -0400

    Finsihed example

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,10 @@ import React from 'react';
 class ImageList extends React.Component {
 
   render() {
-    return <div>ImageList!</div>
+    let images = this.props.images.map( i =>{
+      return  <img key={i.id} src={i.urls.regular} alt={i.urls.regular} />
+    })
+    return <div>{images}</div>
   }
 }
 
